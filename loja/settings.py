4d9f90001a3d4aab246a9f36af00a05d69c0195e
rodrigo <rodrigog3wconcept@gmail.com>
2025-04-10 15:30:05 -0300

    feat: created cadastro usuarios pt 1

--- loja/settings.py
+++ loja/settings.py
@@ -39,15 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'crispy_forms',
+    "crispy_bootstrap4",
     'pedido',
     'perfil',
     'produto',
 
-
     # TODO: Remover debug toolbar
     'debug_toolbar',
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- perfil/views.py
+++ perfil/views.py
@@ -11,24 +11,43 @@ from . import forms
 class BasePerfil(View):
     template_name = 'perfil/criar.html'
 
-    def setuo(self, *args, **kwargs):
+    def setup(self, *args, **kwargs):
         super().setup(*args, **kwargs)
 
-        self.contexto = {
-            'userform': forms.UserForm(data=self.request.POST or None),
-            'perfilform': forms.PerfilForm(data=self.request.POST or None)
-        }
+        if self.request.user.is_authenticated:
+            self.contexto = {
+                'userform': forms.UserForm(
+                    data=self.request.POST or None,
+                    usuario=self.request.user,
+                    instance=self.request.user,
+                ),
+                'perfilform': forms.PerfilForm(
+                    data=self.request.POST or None
+                    )
+            }
 
-        self.renderizar = render(self.request, self.template_name, self.contexto)
+        else:
+            self.contexto = {
+                'userform': forms.UserForm(
+                    data=self.request.POST or None
+                 ),
+                'perfilform': forms.PerfilForm(
+                    data=self.request.POST or None
+                )
+            }
+
+        self.renderizar = render(
+            self.request, self.template_name, self.contexto)
 
     def get(self, *args, **kwargs):
-        return HttpResponse('Criar')
+        return self.renderizar
 
 
 
 
 class Criar(BasePerfil):
-    pass
+    def post(self, *args, **kwargs):
+        return self.renderizar
 
 class Atualizar(View):
     def get(self, *args, **kwargs):
